d11cd709495ec3c07b8df80e7ca897a0ac4e1e00
Klemen Slavič <krof.drakula@gmail.com>
2014-02-13 17:05:01 -0800

    Sketching out structure

--- static/01/lib/game.js
+++ static/01/lib/game.js
@@ -1,7 +1,70 @@
 $(function() {
 
     var container = $('#viewport');
-
     
+    var width  = container.width();
+    var height = container.height();
+
+    var clouds      = [];
+    var targets     = [];
+    var player      = null;
+    var camera      = null;
+    var focalLength = height;
+
+    var cloudCount  = 50;
+    var targetCount = 20;
+    var trackLength = 200;
+
+
+    function init() {
+        var i;
+        // generate random clouds, front to back
+        for (i = 0; i < cloudCount; i++) {
+            clouds.push(
+                new GameObject(
+                    new Vec3(
+                        (Math.random() - 0.5) * width,
+                        (Math.random() - 0.5) * height,
+                        (i / cloudCount) * width
+                    ),
+                    getImage('cloud')
+                )
+            );
+        }
+
+        // generate random targets, front to back
+        for (i = 0; i < targetCount; i++) {
+            targets.push(
+                new GameObject(
+                    new Vec3(
+                        (Math.random() - 0.5) * width,
+                        (Math.random() - 0.5) * height,
+                        ((i + 1) / targetCount) * width * (trackLength / targetCount)
+                    ),
+                    getImage('target')
+                )
+            );
+        }
+
+        // start the player in the center of the screen and at zero distance
+        player = new Vec3(0, 0, 0);
+
+        // the camera should start off behind the player, at a distance
+        // of focalLength
+        camera = new Vec3(player.x, player.y, player.z - focalLength);
+    }
+
+    function getImage(name) {
+        var img = document.createElement('img');
+        img.src = '/assets/' + name + '.png';
+        container.appendChild(img);
+        return img;
+    }
+
+    function populateClouds() {
+        clouds.forEach(function(cloud) {
+
+        });
+    }
 
 });
---
+++ static/01/lib/game_objects.js
@@ -0,0 +1,8 @@
+function GameObject(pos, graphic) {
+    this.pos = pos;
+    this.graphic = graphic;
+}
+
+GameObject.prototype.render = function(camera) {
+    // TODO: DOM rendered object
+};
