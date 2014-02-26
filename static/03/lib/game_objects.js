2ae7d9fd940a5116b9557e17ee2e93dcdee73d6f
Klemen Slavič <krof.drakula@gmail.com>
2014-02-26 07:39:28 +0100

    Fuck yeah 3d transforms

---
+++ static/03/lib/game_objects.js
@@ -0,0 +1,32 @@
+function GameObject(pos, graphic, props) {
+    this.pos     = pos;
+    this.graphic = graphic;
+    this.props   = props || {};
+    this.width   = graphic.get(0).naturalWidth;
+    this.height  = graphic.get(0).naturalHeight;
+    this.render  = this.render.bind(this);
+}
+
+EventEmitter.mixin(GameObject.prototype);
+
+GameObject.prototype.render = function(camera, view) {
+    var cPos = this.pos.sub(camera.pos);
+    if (cPos.z <= 0) {
+        this.graphic.css({display: 'none'});
+    } else {
+        var k = camera.f / cPos.z,
+            x = (cPos.x - this.width  / 2) * k + view.width  / 2,
+            y = (cPos.y - this.height / 2) * k + view.height / 2;
+        this.graphic.css({
+            display         : 'block',
+            transform       : 'translate3d(' + x + 'px, ' + y + 'px, 0) scale(' + k + ')',
+            transformOrigin : '0 0',
+            opacity         : Math.min(cPos.z / camera.f, 5/4-1/(4*camera.f)*cPos.z),
+            zIndex          : Math.round(1e8 - cPos.z)
+        });
+    }
+};
+
+GameObject.prototype.getBoundingRect = function() {
+    return this.graphic.get(0).getBoundingClientRect();
+};
