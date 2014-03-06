1f1421bf014e2530e94a0611f4739799420875cf
Klemen Slavič <krof.drakula@gmail.com>
2014-03-06 12:48:12 +0100

    Implemented rotation in canvas

--- static/06/lib/game_objects.js
+++ static/06/lib/game_objects.js
@@ -13,14 +13,20 @@ EventEmitter.mixin(GameObject.prototype);
 GameObject.prototype.render = function(camera, view, ctx) {
     var z = this.pos.z - camera.pos.z,
         k = camera.f / z,
-        x = (this.pos.x - camera.pos.x - this.width / 2) * k + view.width / 2,
-        y = (this.pos.y - camera.pos.y - this.height / 2) * k + view.height / 2,
+        x = (this.pos.x - camera.pos.x) * k + view.width / 2,
+        y = (this.pos.y - camera.pos.y) * k + view.height / 2,
         w = this.width * k,
         h = this.height * k;
 
     if (z > 0) {
+        ctx.save();
         ctx.globalAlpha = Math.min(z / camera.f, 5/4-1/(4*camera.f)*z);
-        ctx.drawImage(this.graphic, x, y, w, h);
+
+        ctx.translate(x, y);
+        ctx.rotate(-this.rotation);
+        ctx.drawImage(this.graphic, -w/2, -h/2, w, h);
+
+        ctx.restore();
         ctx.globalAlpha = 1;
     }
 };
